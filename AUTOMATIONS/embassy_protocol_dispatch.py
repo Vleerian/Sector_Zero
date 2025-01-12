c7ed78c23ccc5f65e77323a1c369d891e5767799
Vleerian Remordias <vleerian@hotmail.com>
2025-01-12 10:24:24 -0500

    ratify(i/o): Add dispatch uploader, update embassy dispatch generator to output needed data

--- AUTOMATIONS/embassy_protocol_dispatch.py
+++ AUTOMATIONS/embassy_protocol_dispatch.py
@@ -1,19 +1,34 @@
 import sys
 import csv
 
-header = """[background-block=#c30000][hr][center][img]https://i.imgur.com/9J1Rqfs.png[/img][/center][hr][/background-block]
+"""
+This generates a dispatch upload file using the CSV output of score_embassies.py
+The output has two primary sections, the header and content
+Headers are formatted with CLRF at the end of each line, similarly to HTTP
+The Content is partitioned with two CLRFs, same as HTTP
+
+usage: python embassy_protocol_dispatch.py CHECKSUM DISPATCH_ID
+"""
+
+Header = f"[HIGHSEC] S0 Embassy Policy Test\r\n{sys.argv[2]},1,109\r\n\r\n"
+
+Content = """[background-block=#c30000][hr][center][img]https://i.imgur.com/9J1Rqfs.png[/img][/center][hr][/background-block]
 [b][size=150][color=c30000]Granted Embassy Protocols[/color][/size][/b]
 [table][tr][td]Grant Index[/td][td]Region[/td][td]Diagnostic Score[/td][td]Ratification Commit[/td][/tr]
 """
-footer = f"""
-[/table]
-[hr]
-version: {sys.argv[1]}"""
+
 embProto = [Tag for Tag in csv.DictReader(open("PRIVATE/EMBASSY PROTOCOLS.csv", "r"))]
 rows = []
 for i in range(0, len(embProto)):
     rows.append(f"[tr][td]{i}[/td][td][region]{embProto[i]["embassy"]}[/region][/td][td]{embProto[i]["score"]}[/td][td][color=cyan]{embProto[i]["checksum"]}[/color][/td][/tr]")
 
-Dispatch = header + "\n".join(rows) + footer
-with open("PUBLIC/GRANTED_EMBASSY_PROTOCOLS.bb", "w+") as file:
-    file.write(Dispatch)
+Content += "\n".join(rows)
+
+Content += f"""
+[/table]
+[hr]
+version: {sys.argv[1]}"""
+
+with open("PUBLIC/GRANTED_EMBASSY_PROTOCOLS.SZD", "w+") as file:
+    file.write(Header+Content)
+print(Header+Content)
---
+++ AUTOMATIONS/upload_dispatch.py
@@ -0,0 +1,47 @@
+import requests
+import urllib.parse
+import re
+import sys
+import time
+
+"""
+This takes the formatted output from 
+"""
+startTiime = time.time()
+def log(type: str, message: str):
+    runtime = int(time.time() - startTiime)
+    print(f'{runtime:05} [{type}] {message}')
+
+log("INFO", f"Loading and processing {sys.argv[3]}")
+# Split the dispatch file at the CRLFs to get the header and body
+header, body, *other = open(sys.argv[3], "rb").read().split(b"\r\n\r\n")
+# The header is formatted as: TitleCRLFDispatchID,Section,Subsection
+headerSplit = header.split(b"\r\n")
+dispatchTitle = str(urllib.parse.quote(headerSplit[0]))
+idInfo = headerSplit[1].split(b",")
+dispatchID = int(idInfo[0])
+category = int(idInfo[1])
+subcategory = int(idInfo[2])
+# cut out extra whitespace off the body and format it for upload
+dispatchText = str(urllib.parse.quote(body.strip().replace(b"\n",b"")))
+
+log("INFO", f"Preparing to upload to discpatch#{dispatchID}@{sys.argv[2]}")
+log("INFO", f"Title: {str(headerSplit[0])}")
+# Build out the URIs and UserAgent
+userAgent = f"Sector_Zero_Dispatch_Uploader (vleerian on discord, vleerian@hotmail.com, in use by {sys.argv[1]})"
+baseAPI = f"https://www.nationstates.net/cgi-bin/api.cgi?nation={sys.argv[2]}"
+
+editDispatchPart = f"&c=dispatch&dispatch=edit&dispatchid={dispatchID}&title={dispatchTitle}&category={category}&subcategory={subcategory}"
+editDispatchPart += f"&text={dispatchText}"
+
+log("REQX", "Sending prepare request...")
+r = requests.get(baseAPI + editDispatchPart + "&mode=prepare",
+    headers={ "User-Agent":userAgent, "X-Password":sys.argv[4] })
+token = re.findall("S>([^<]*)<", str(r.content))[0]
+log("GOOD", "Token acquired.")
+
+log("REQX", "Sending prepare request...")
+xPin = r.headers["X-Pin"]
+r = requests.get(baseAPI + editDispatchPart + f"&mode=execute&token={token}",
+    headers={ "User-Agent":userAgent, "X-Pin":xPin })
+log("GOOD", "UPLOAD COMPLETE")
